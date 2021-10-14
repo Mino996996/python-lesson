e78ff09624a689ed58fc4a31b156696881e964c3
minoru akahori <minoruaka@gmail.com>
2021-10-14 15:34:45 +0900

    update

--- free_lesson.py
+++ free_lesson.py
@@ -2,3 +2,11 @@ a, b = [5, 3]
 
 print(a)
 print(b)
+
+
+dic = {"nest": {"nes1": 123, "nes2": 456, "nes3": 789}}
+
+print(dic["nest"])
+print(len(dic["nest"]))
+print(dic["nest"]["nes2"])
+print(len(str(dic["nest"]["nes2"])))
